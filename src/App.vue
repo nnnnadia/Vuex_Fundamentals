687c9f3e7056b001398e59ca4b2e96e95c8732c8
nnnnadia <tristaonadia@gmail.com>
2024-01-23 15:58:28 -0300

    create-vue app then packages installation

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,85 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'EventList' }">Events</router-link> |
-      <router-link :to="{ name: 'About' }">About</router-link>
-       |
-      <router-link :to="{ name: 'EventCreate' }">Create Event</router-link>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
     </div>
-    <router-view />
-  </div>
+  </header>
+
+  <RouterView />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-button {
-  align-items: center;
-  height: 50px;
-  padding: 0 40px;
-  background: linear-gradient(to right, #16c0b0, #84cf6a);
-  color: #ffffff;
-  border: none;
-  border-radius: 6px;
-  font-size: 18px;
-  font-weight: 800;
-  cursor: pointer;
-  transition: all 0.2s linear;
-}
-
-button:hover {
-  -webkit-transform: scale(1.02);
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-
-h2 {
-  font-size: 40px;
-  font-weight: 700;
-}
-
-h3 {
-  font-size: 25px;
-  font-weight: 700;
-}
-
-h4 {
-  font-size: 20px;
-  font-weight: 700;
-}
-
-input {
+.logo {
   display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
   width: 100%;
-  height: 30px;
-  margin-bottom: 24px;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
 
-input[type=text] {
-  padding: 0px 10px;
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
 
-input:focus {
-  border-color: #16c0b0;
-  outline: 0;
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
 
-
-#nav {
-  padding: 30px;
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+nav a:first-of-type {
+  border: 0;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
 
+  .logo {
+    margin: 0 2rem 0 0;
+  }
 
-.form-container {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 10%;
-}
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 
-select:focus {
-  border-color: #16c0b0;
-  outline: 0;
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EventList from '../views/EventList.vue'
-import EventDetails from '../views/EventDetails.vue'
-import EventCreate from '../views/EventCreate.vue'
-import ErrorDisplay from '../views/ErrorDisplay.vue'
-import About from '../views/About.vue'
-
-const routes = [
-  {
-    path: '/',
-    name: 'EventList',
-    component: EventList
-  },
-  {
-    path: '/event/:id',
-    name: 'EventDetails',
-    props: true,
-    component: EventDetails
-  },
-  {
-    path: '/event/create',
-    name: 'EventCreate',
-    component: EventCreate
-  },
-  {
-    path: '/error/:error',
-    name: 'ErrorDisplay',
-    props: true,
-    component: ErrorDisplay
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: About
-  }
-]
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
